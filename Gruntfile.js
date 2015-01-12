88e2d18db03c46bc08c95d7128de157dceeabc8a
Chris Hutchinson <chris_hutchinson@me.com>
2015-01-12 11:58:33 +0000

    Update package.json, implement grunt-build-control

--- Gruntfile.js
+++ Gruntfile.js
@@ -384,6 +384,21 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:times/cardkit-v2.git',
+          branch: 'gh-pages'
+        }
+      },
     }
   });
 

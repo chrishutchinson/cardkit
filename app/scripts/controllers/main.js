bab5beebb90ab339e05882f2c1b528bfc46663b9
Chris Hutchinson <chris_hutchinson@me.com>
2015-02-25 14:53:54 +0000

    Update style for Times Woman

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,158 +13,94 @@ angular.module('cardkitApp')
     $scope.config = {
       sizes: [
         {
-          name: 'Facebook',
-          width: 800,
-          height: 370,
-        },
-        {
-          name: 'Twitter',
-          width: 650,
-          height: 320,
-        },
-        {
-          name: 'Video',
-          width: 640,
-          height: 360,
-        },
+          name: 'Email',
+          width: 1000,
+          height: 330,
+        }
       ],
       themes: themeConfig,
-      output: {
-        scale: 2,
-        editable: {
-          scale: true
-        }
-      },
       svg: {
-    		canvas: {
-    			height: function() {
+        canvas: {
+          height: function() {
             return $scope.size.height;
           },
-    			width: function() {
+          width: function() {
             return $scope.size.width;
           },
-    		},
-    		elements: [
+          fill: '#FFFFFF'
+        },
+        elements: [
           {
-            name: 'Background Colour',
-            type: 'rect',
-            height: function() {
-              return $scope.size.height;
-            },
+            name: 'Background',
+            type: 'image',
             width: function() {
               return $scope.size.width;
             },
-            fill: function() {
-              return $scope.theme.background;
+            height: function() {
+              return $scope.size.height;
             },
-            editable: {
-              fill: 'picker'
-            }
+            src: function() {
+              return $scope.theme.backgroundSrc;
+            },
+            opacity: 1,
+            x: '0%',
+            y: '0%',
+            draggable: false,
+            editable: false
           },
           {
             name: 'Image',
             type: 'image',
-            width: 600,
+            width: 200,
             height: function() {
               return this.width;
             },
             src: '',
             opacity: 1,
-            x: '0%',
-            y: '0%',
+            x: '50%',
+            y: '50%',
             preserveAspectRatio: 'xMinYMin meet',
             draggable: true,
-            defaultFilter: '',
             editable: {
               src: true,
               width: true,
-              opacity: true,
-              filters: [
-                'Sepia',
-                'Grayscale',
-                'Saturate',
-                'Invert',
-                'Blur'
-              ],
             }
           },
           {
-            name: 'Logo',
-            type: 'image',
-            width: 250,
-            height: function() {
-              return this.width;
+            name: 'Title',
+            type: 'text',
+            text: function() {
+              return $scope.theme.title;
             },
-            src: function() {
-              return $scope.theme.logoSrc;
+            fill: function() {
+              return $scope.theme.color;
             },
-            opacity: 1,
-            x: 50,
-            y: 270,
-            preserveAspectRatio: 'xMinYMin meet',
-            editable: {
-              src: true,
-              width: true,
-            },
-            draggable: true
+            fontSize: 100,
+            fontFamily: '"TimesModern-Bold-Italic"',
+            x: 30,
+            y: 115,
+            draggable: false,
+            editable: false,
           },
           {
-            name: 'Credit',
+            name: 'Title',
             type: 'text',
-            text: 'Credit: Insert name here',
+            text: 'This week: ',
             fill: function() {
-              return $scope.theme.quote;
+              return $scope.theme.color;
             },
-            fontSize: 12,
-            fontFamily: function() {
-              return $scope.theme.headlineFont;
-            },
-            textAnchor: 'start',
-            x: 50,
-            y: 250,
-            draggable: true,
+            fontSize: 50,
+            fontFamily: '"TimesModern-Regular-Italic"',
+            x: 30,
+            y: 190,
+            draggable: false,
             editable: {
-              text: true,
-              fontSize: {
-                'Small': 12,
-                'Medium': 18,
-                'Large': 22,
-                'Extra Large': 36,
-              },
-              fill: 'picker',
-              textAnchor: true
+              text: true
             },
           },
-          {
-            name: 'Headline',
-            type: 'text',
-            text: 'Edit this text, and drag it around.\n\nYou can upload your own background image,\nlogo, and change the colour of the text too.',
-            fill: function() {
-              return $scope.theme.quote;
-            },
-            fontSize: 26,
-            fontFamily: function() {
-              return $scope.theme.headlineFont;
-            },
-            textAnchor: 'start',
-            x: 50,
-            y: 55,
-            draggable: true,
-            editable: {
-              text: true,
-              fill: 'picker',
-              textAnchor: true,
-              fontSize: {
-                'Small (18px)': 18,
-                'Medium (26px)': 26,
-                'Large (32px)': 32,
-                'Extra Large (32px)': 40,
-              },
-            },
-          },
-    		],
+        ],
       }
-  	};
+    };
 
     if(typeof $scope.config.themes !== 'undefined') {
       $scope.theme = ($scope.config.themes.length > 1) ? null : $scope.config.themes[0];

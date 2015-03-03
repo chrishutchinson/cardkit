e9b32851fede946a0ecdb0541493cef641627bb1
Chris Hutchinson <chris_hutchinson@me.com>
2015-03-03 17:59:46 +0000

    Update build and styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -354,6 +354,12 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          dot: true,
+          cwd: 'bower_components/fontawesome',
+          src: ['fonts/*.*'],
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
